acfb5a869e6423324d6c798e83b2df767d8ed126
Gregory Presser <gpress2222@gmail.com>
2022-09-13 09:47:13 -0400

    fixed BCE function

--- hw2/main.py
+++ hw2/main.py
@@ -24,12 +24,11 @@ class Data:
     y: np.ndarray = field(init=False)
     rng: InitVar[np.random.Generator]
 
-
-    def __post_init__(self,rng): 
+    def __post_init__(self, rng):
         """
-        Data Generation based off of https://conx.readthedocs.io/en/latest/Two-Spirals.html
+        Data Generation based off
+        of https://conx.readthedocs.io/en/latest/Two-Spirals.html
         Then Vectorized
-        TODO: Ask if I need to add like variation here
         """
         self.index = np.arange(self.num_samples)
         phi = self.index / 16 * np.pi
@@ -65,12 +64,12 @@ class Dense(tf.Module):
     def __init__(self, neurons: int, is_output:bool = False, name: str = None):
         super().__init__(name=name)
         self.neurons = neurons
-        self.is_output= is_output
+        self.is_output = is_output
         self.__is_built = False
 
     def build(self,rng,inputs:int, points:int):
         self.w = tf.Variable(rng.normal(shape=[inputs,self.neurons]),name = "w")
-        self.b = tf.Variable(rng.normal(shape=[points,self.neurons]),name = "b")
+        self.b = tf.Variable(rng.normal(shape=[1,self.neurons]),name = "b")
         self.__is_built = True
 
     def __call__(self,x):
@@ -79,35 +78,39 @@ class Dense(tf.Module):
         v = x @ self.w + self.b
         return tf.nn.sigmoid(v) if self.is_output else tf.nn.relu(v)
 
+
 class Model(tf.Module):
-    def __init__(self,rng,inputs: int, points: int, nodes:list[Dense], name = None):
+    def __init__(self, rng, inputs: int, points: int, nodes: list[Dense], name=None):
         super().__init__(name=name)
         self.layers = []
         with self.name_scope:
             for node in nodes:
-                node.build(rng,inputs,points)
+                node.build(rng, inputs, points)
                 self.layers.append(node)
                 inputs = node.neurons
-    
-    def __call__(self,x):
+
+    def __call__(self, x):
         value = x
         for node in self.layers:
             value = node(value)
         return value
 
-def loss(y, y_hat): 
+
+def loss(y, y_hat):
     return -y * tf.math.log(y_hat) - (1-y) * tf.math.log(1-y_hat)
 
 
 FLAGS = flags.FLAGS
-flags.DEFINE_integer("num_points",100, "Number of points in each spiral")
+flags.DEFINE_integer("num_points", 100, "Number of points in each spiral")
 flags.DEFINE_integer("batch_size", 16, "Number of samples in batch")
 flags.DEFINE_integer("random_seed", 31415, "Random seed")
 flags.DEFINE_float("learning_rate", 0.01, "Learning rate")
 flags.DEFINE_integer("num_iters", 300, "Number of SGD iterations")
 
-def convert_to_color(val: float): 
+
+def convert_to_color(val: float):
     return "blue" if val <= .5 else "red"
+
 
 def main(_):
     seed_sequence = np.random.SeedSequence(FLAGS.random_seed)
@@ -115,49 +118,46 @@ def main(_):
     np_rng = np.random.default_rng(np_seed)
     tf_rng = tf.random.Generator.from_seed(tf_seed.entropy)
 
-    d = Data(FLAGS.num_points,np_rng)
+    d = Data(FLAGS.num_points, np_rng)
 
     model = Model(tf_rng,
-            input=2,
-            points=FLAGS.batch_size,
-            nodes=[
-                Dense(32),
-                Dense(32), 
-                Dense(32), 
-                Dense(32), 
-                Dense(1,True), 
-            ]
-        )
+                  inputs=2,
+                  points=FLAGS.batch_size,
+                  nodes=[
+                    Dense(32),
+                    Dense(32),
+                    Dense(32),
+                    Dense(32),
+                    Dense(1, True)
+                  ])
 
     optimizer = tf.keras.optimizers.Adam(learning_rate=.01)
 
     bar = trange(FLAGS.num_iters)
-    for _ in bar:
+    for i in bar:
         with tf.GradientTape() as tape:
-            x_train,y_train = d.get_batch(np_rng,FLAGS.batch_size)
-            # print(y_train.shape)
+            x_train, y_train = d.get_batch(np_rng, FLAGS.batch_size)
             y_hat = model(x_train)
-            # print(y_hat.shape, y_train.shape)
-            ls = loss(y_train.reshape(16,1),y_hat)
-            # print("loss: ", ls.shape, ls)
-            # TODO fix only getting nans 
+            loss = tf.keras.losses.BinaryCrossentropy()
+            ls = loss(y_train.reshape(16, 1), y_hat)
 
         grads = tape.gradient(ls, model.trainable_variables)
         optimizer.apply_gradients(zip(grads, model.trainable_variables))
 
-        # bar.set_description(f"Loss @ {i} => {np.average(ls).reduce:0.6f}") # TODO fix this 
+        bar.set_description(f"Loss @ {i} => {ls.numpy():0.6f}")
         bar.refresh()
 
-    # true_colors = [convert_to_color(y) for y in d.y]
-    # predictions = [convert_to_color(model.predict(points.reshape(2,1)).flatten()[0]) for points in d.x]
-    # fig, ax = plt.subplots(1, 2, figsize=(10, 3), dpi=200)
-    # ax[0].set_title("true")
-    # ax[0].scatter(d.x[::,0],d.x[::,1],  color=true_colors)
-    # ax[1].set_title("predictions")
-    # ax[1].scatter(d.x[::,0],d.x[::,1],  color=predictions)
+    true_colors = [convert_to_color(y) for y in d.y]
+    predictions = [convert_to_color(color) for color in model(d.x).numpy()]
+    fig, ax = plt.subplots(1, 2, figsize=(10, 3), dpi=200)
+    ax[0].set_title("true")
+    ax[0].scatter(d.x[::, 0], d.x[::, 1], color=true_colors)
+    ax[1].set_title("predictions")
+    ax[1].scatter(d.x[::, 0], d.x[::, 1], color=predictions)
 
     plt.tight_layout()
     plt.savefig(f"{script_path}/fit.pdf")
+
 
 if __name__ == "__main__":
     app.run(main)
