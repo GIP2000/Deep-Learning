de1aa6afa41a6ac4da06762af1da2cfd4d1a0c68
Gregory Presser <gpress2222@gmail.com>
2022-09-24 21:59:33 -0400

    Made it better, need to test it but computer is too hot right now

--- hw4/main.py
+++ hw4/main.py
@@ -2,11 +2,12 @@ import numpy as np
 import matplotlib.pyplot as plt
 import tensorflow as tf
 from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Conv2D, MaxPooling2D, Dense, Flatten,Dropout
+from tensorflow.keras.layers import Conv2D, MaxPooling2D, Dense, Flatten, Dropout, ReLU, BatchNormalization
 
 FOLDER_NAME = "data/"
 FILE_NAME = "data_batch_"
 FILE_COUNT = 5
+TEST_FILE = "test_batch"
 META_FILE = "batches.meta"
 RNG_SEED = 31415926
 LEARNING_RATE = .001
@@ -14,40 +15,32 @@ BATCH_SIZE = 128
 EPOCHS = 50
 
 
-def get_meta(file):
+def get_meta(file: str):
     return unpickle(file)[b'label_names']
 
 
-def load_batch(file):
+def load_batch(file: str):
     dict = unpickle(file)
-    return dict[b'data'].reshape(len(dict[b'labels']),3,32,32).transpose(0,2,3,1), dict[b'labels']
+    return dict[b'data'].reshape(len(dict[b'labels']), 3, 32, 32).transpose(0, 2, 3, 1),\
+        np.array(tf.keras.utils.to_categorical(dict[b'labels']))
 
 
-def load_data(file_base, batches):
-    x = []
-    y = []
-    for i in range(batches - 1):
-        x_t, y_t = load_batch(file_base + str(i+1))
-        x.append(x_t)
-        y.append(tf.keras.utils.to_categorical(y_t))
-    x_val, y_val = load_batch(file_base + str(batches - 1))
-    return np.concatenate(x), np.concatenate(y), x_val, np.array(tf.keras.utils.to_categorical(y_val))
-
-
-def unpickle(file):
+def unpickle(file: str):
     import pickle
     with open(file, 'rb') as fo:
         dict = pickle.load(fo, encoding='bytes')
     return dict
 
-def split(data: np.ndarray, label: np.array, rng):
-    indexes = rng.permutation(len(label))
-    r_data = data[indexes]
-    r_label = label[indexes]
-    validiation_split = int(.8 * len(r_label))
 
-    return (r_data[0:validiation_split]), tf.keras.utils.to_categorical(r_data[0:validiation_split]), \
-        (r_data[validiation_split::]), tf.keras.utils.to_categorical(r_label[validiation_split::])
+def load_data(file_base: str, batches: int):
+    x = []
+    y = []
+    # well I can't vectorize IO operations so here is a gross for loop
+    for i in range(batches):
+        x_t, y_t = load_batch(file_base + str(i+1))
+        x.append(x_t)
+        y.append(y_t)
+    return np.concatenate(x[0:-1]), np.concatenate(y[0:-1]), x[-1], y[-1]
 
 
 def plotImg(img: np.ndarray, label: str):
@@ -57,28 +50,35 @@ def plotImg(img: np.ndarray, label: str):
 
 
 def main():
-    seed_sequence = np.random.SeedSequence(RNG_SEED)
-    [np_seed] = seed_sequence.spawn(1)
-    np_rng = np.random.default_rng(np_seed)
-
     # names = get_meta(FOLDER_NAME + META_FILE)
-    x_train, y_train, x_val, y_val = load_data(FOLDER_NAME + FILE_NAME, FILE_COUNT)
+    x_train, y_train, x_val, y_val = load_data(
+        FOLDER_NAME + FILE_NAME, FILE_COUNT)
 
     model = Sequential([
-        Conv2D(32, (3, 3), activation='relu', input_shape=(32, 32, 3)),
+        Conv2D(256, (3, 3), activation='relu', input_shape=(32, 32, 3)),
+        MaxPooling2D((2, 2)),
+        BatchNormalization(),
+        ReLU(),
+        Conv2D(256, (3, 3),  activation='relu'),
         MaxPooling2D((2, 2)),
         Dropout(.2),
-        Conv2D(64, (3, 3),  activation='relu'),
-        MaxPooling2D((2, 2)),
-        Conv2D(64, (3, 3), activation='relu'),
+        Conv2D(256, (3, 3), activation='relu'),
         Flatten(),
-        Dense(64, activation='relu'),
+        Dense(256, activation='relu'),
+        Dense(256, activation='relu'),
+        Dense(256, activation='relu'),
         Dense(10, activation='softmax', kernel_regularizer='l2')
     ])
 
     model.summary()
-    model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
-    history = model.fit(x_train, y_train, epochs=EPOCHS, batch_size=BATCH_SIZE, validation_data=(x_val, y_val))
+    model.compile(optimizer='adam',
+                  loss='categorical_crossentropy', metrics=['accuracy'])
+    history = model.fit(x_train, y_train, epochs=EPOCHS,
+                        batch_size=BATCH_SIZE, validation_data=(x_val, y_val))
+
+    x_test, y_test = load_batch(FOLDER_NAME + TEST_FILE)
+    _, acc = model.evaluate(x_test, y_test)
+    print("acc: " + str(acc))
 
 
 if __name__ == "__main__":
