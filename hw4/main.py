e9fe711a29e8e3042bd26615541b17e57c4a40c9
Gregory Presser <gpress2222@gmail.com>
2022-09-28 19:20:20 -0400

    cifar100 whoops

--- hw4/main.py
+++ hw4/main.py
@@ -6,7 +6,6 @@ from tensorflow.keras.layers import Conv2D, MaxPooling2D, Dense,\
     Flatten, Dropout, ReLU, BatchNormalization
 import tensorflow_addons as tfa
 
-FOLDER_NAME = "data/"
 FILE_NAME = "data_batch_"
 FILE_COUNT = 5
 TEST_FILE = "test_batch"
@@ -21,10 +20,10 @@ def get_meta(file: str):
     return unpickle(file)[b'label_names']
 
 
-def load_batch(file: str):
+def load_batch(file: str, label_key:str = b'labels'):
     dict = unpickle(file)
-    return dict[b'data'].reshape(len(dict[b'labels']), 3, 32, 32).transpose(0, 2, 3, 1),\
-        np.array(tf.keras.utils.to_categorical(dict[b'labels']))
+    return dict[b'data'].reshape(len(dict[label_key]), 3, 32, 32).transpose(0, 2, 3, 1),\
+        np.array(tf.keras.utils.to_categorical(dict[label_key]))
 
 
 def unpickle(file: str):
@@ -51,30 +50,81 @@ def plotImg(img: np.ndarray, label: str):
     plt.show()
 
 
-def main():
+def cifar100():
+    seed_sequence = np.random.SeedSequence(RNG_SEED)
+    [np_seed] = seed_sequence.spawn(1)
+    np_rng = np.random.default_rng(np_seed)
+    x, y = load_batch("cifar-100-python/test", b'fine_labels')
+    print(x.shape)
+    print(y.shape)
+    indexes = np_rng.permutation(len(y))
+    x_r = x[indexes]
+    y_r = y[indexes]
+    split = int(.8*len(y))
+    x_train = x_r[0::split]
+    y_train = y_r[0::split]
+    x_val = x_r[split::]
+    y_val = y_r[split::]
+
+
+    model = Sequential([
+        Conv2D(256, (2, 2), activation='relu', input_shape=(32, 32, 3)),
+        MaxPooling2D((2, 2)),
+        BatchNormalization(),
+        ReLU(),
+        Conv2D(256, (2, 2),  activation='relu'),
+        Conv2D(256, (2, 2),  activation='relu'),
+        Conv2D(256, (2, 2),  activation='relu'),
+        MaxPooling2D((2, 2)),
+        Conv2D(256, (2, 2), activation='relu'),
+        Conv2D(256, (2, 2), activation='relu'),
+        Flatten(),
+        Dense(256, activation='relu'),
+        Dense(256, activation='relu'),
+        Dropout(.6),
+        Dense(256, activation='relu'),
+        Dense(256, activation='relu'),
+        Dropout(.6),
+        Dense(10, activation='softmax', kernel_regularizer='l2')
+    ])
+
+    optimizer = tfa.optimizers.AdamW(
+        weight_decay=1e-6, learning_rate=LEARNING_RATE)
+    model.summary()
+    model.compile(optimizer=optimizer,
+                  loss='categorical_crossentropy', metrics=['accuracy'])
+    history = model.fit(x_train, y_train, epochs=EPOCHS,
+                        batch_size=BATCH_SIZE, validation_data=(x_val, y_val))
+
+    x_test, y_test = load_batch("cifar-100-python/test", b'fine_labels')
+    _, acc = model.evaluate(x_test, y_test)
+    print("acc: " + str(acc))
+
+
+def cifar10():
+    FOLDER_NAME = "data/"
     # names = get_meta(FOLDER_NAME + META_FILE)
     x_train, y_train, x_val, y_val = load_data(
         FOLDER_NAME + FILE_NAME, FILE_COUNT)
 
     model = Sequential([
-        Conv2D(256, (3, 3), activation='relu', input_shape=(32, 32, 3)),
+        Conv2D(256, (2, 2), activation='relu', input_shape=(32, 32, 3)),
         MaxPooling2D((2, 2)),
         BatchNormalization(),
         ReLU(),
-        Conv2D(512, (3, 3),  activation='relu'),
-        Conv2D(512, (3, 3),  activation='relu'),
-        Conv2D(512, (3, 3),  activation='relu'),
+        Conv2D(256, (2, 2),  activation='relu'),
+        Conv2D(256, (2, 2),  activation='relu'),
+        Conv2D(256, (2, 2),  activation='relu'),
         MaxPooling2D((2, 2)),
-        Conv2D(512, (3, 3), activation='relu'),
-        Conv2D(512, (3, 3), activation='relu'),
-        Conv2D(512, (3, 3), activation='relu'),
+        Conv2D(256, (2, 2), activation='relu'),
+        Conv2D(256, (2, 2), activation='relu'),
         Flatten(),
         Dense(256, activation='relu'),
         Dense(256, activation='relu'),
-        Dropout(.3),
+        Dropout(.6),
         Dense(256, activation='relu'),
         Dense(256, activation='relu'),
-        Dropout(.2),
+        Dropout(.6),
         Dense(10, activation='softmax', kernel_regularizer='l2')
     ])
 
@@ -89,6 +139,11 @@ def main():
     x_test, y_test = load_batch(FOLDER_NAME + TEST_FILE)
     _, acc = model.evaluate(x_test, y_test)
     print("acc: " + str(acc))
+ 
+
+def main():
+    # cifar10()
+    cifar100()
 
 
 if __name__ == "__main__":
